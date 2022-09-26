872e91578d1867e75bf81ed722c8c130c87c91a3
leonardo <106268245+leoz11@users.noreply.github.com>
2022-09-26 17:08:29 -0300

    Update files

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+export const api = "https://clonegramreact.herokuapp.com/api";
+export const uploads = "https://clonegramreact.herokuapp.com/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
